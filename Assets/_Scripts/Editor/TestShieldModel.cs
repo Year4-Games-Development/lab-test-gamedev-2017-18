7525c654b161abddda1aeb683b7f44bd72b03027
B00078871 Kevin Lardner <B00078871@student.itb.ie>
2018-01-12 15:28:58 +0000

    Upload Test Files Before Issue With PC

---
+++ Assets/_Scripts/Editor/TestShieldModel.cs
@@ -0,0 +1,90 @@
+﻿using NUnit.Framework;
+public class TestShieldModel
+{
+
+
+	/*
+	[Test]
+	public void TestFred()
+	{
+		// Arrange
+		ShieldModel sm = new ShieldModel();
+		int damage = 90;
+		int strength = 10;
+
+		// Act
+		int result = (sm.GetStrength()-damage);
+		bool repairSuccess = sm.IsRepairable();
+		//int result = (sm.GetStrength()-damage);
+
+		// Assert
+		Assert.False(repairSuccess);
+		Assert.AreEqual(strength,result);
+	}
+
+
+
+
+	[Test]
+	public void TestAfterTenDamageRepairIsTrueAndStrengthIsNinty()
+	{
+		// Arrange
+		ShieldModel sm = new ShieldModel();
+		int damage = 10;
+		int strength = 90;
+
+		// Act
+		bool repairSuccess = sm.IsRepairable();
+		//int result = (sm.GetStrength()-damage);
+		int result = sm.TakeDamage(damage);
+
+		// Assert
+		Assert.True(repairSuccess);
+		Assert.AreEqual(strength,result);
+	}
+
+	[Test]
+	public void TestAfterEightTimesOfTenDamageRepairIsFalseAndStrengthIsTwenty()
+	{
+		// Arrange
+		ShieldModel sm = new ShieldModel();
+		int damage = 10;
+		int times = 8;
+		int strength = 20;
+
+		// Act
+		int totalDamage = damage * times;
+		bool repairSuccess = sm.IsRepairable();
+		//int result = (sm.GetStrength()-damage);
+		int result = sm.TakeDamage(totalDamage);
+
+		// Assert
+		Assert.False(repairSuccess);
+		Assert.AreEqual(strength,result);
+	}
+
+	[Test]
+	public void TestAfterEightTimesOfTenDamageRepairIsFalseAndStrengthIsTwenty()
+	{
+		// Arrange
+		ShieldModel sm = new ShieldModel();
+		int damage = 10;
+		int times = 8;
+		int strength = 20;
+
+		// Act
+		int totalDamage = damage * times;
+		bool repairSuccess = sm.IsRepairable();
+		//int result = (sm.GetStrength()-damage);
+		int result = sm.TakeDamage(totalDamage);
+
+		// Assert
+		Assert.False(repairSuccess);
+		Assert.AreEqual(strength,result);
+	}
+
+
+
+*/
+
+}
---
+++ Assets/_Scripts/Editor/TestShieldModel2.cs
@@ -0,0 +1,109 @@
+﻿using NUnit.Framework;
+public class TestShieldModel2
+{
+
+
+	/*
+	[Test]
+	public void TestAfterOneDamageRepairIsTrueAndStrengthIsNintyNine()
+	{
+		// Arrange
+		ShieldModel sm = new ShieldModel();
+		int damage = 1;
+		int strength = 99;
+
+		// Act
+		bool repairSuccess = sm.IsRepairable();
+		int result = (sm.GetStrength()-damage);
+
+		// Assert
+		Assert.True(repairSuccess);
+		Assert.AreEqual(strength,result);
+	}
+
+
+
+	*/
+
+
+	[Test]
+	public void TestAfter1DamageRepairIsTrueAndStrengthIs99Version2()
+	{
+		// Arrange
+		ShieldModel sm = new ShieldModel();
+		int damage = 1;
+		int strength = 99;
+
+		// Act
+		bool repairSuccess = sm.IsRepairable();
+		//int result = (sm.GetStrength()-damage);
+		int result = sm.TakeDamage(damage);
+
+		// Assert
+		Assert.True(repairSuccess);
+		Assert.AreEqual(strength,result);
+	}
+
+
+	[Test]
+	public void TestStrengthIsOneHundred()
+	{
+		// Arrange
+		ShieldModel sm = new ShieldModel();
+		int expectedResult = 100;
+
+		// Act
+		int result = sm.GetStrength ();
+
+		// Assert
+		Assert.AreEqual (expectedResult, result);
+	}
+
+
+
+	[Test]
+	public void TestRepairQuantityIsTen()
+	{
+		// Arrange
+		ShieldModel sm = new ShieldModel();
+		int expectedResult = 10;
+
+		// Act
+		int result = sm.RepairQuantity;
+
+		// Assert
+		Assert.AreEqual (expectedResult, result);
+	}
+
+	/*
+
+	[Test]
+	public void TestRepairthresholdIsTwentyFive()
+	{
+		// Arrange
+		ShieldModel sm = new ShieldModel();
+		int expectedResult = 25;
+
+		// Act
+		int result = sm.RepairableThreshold;
+
+		// Assert
+		Assert.AreEqual (expectedResult, result);
+	}
+
+	[Test]
+	public void TestIsRepairableIsTrue()
+	{
+		// Arrange
+		ShieldModel sm = new ShieldModel();
+
+		// Act
+		bool result = sm.IsRepairable();
+
+		// Assert
+		Assert.True(result);
+	}
+
+*/
+
+}
