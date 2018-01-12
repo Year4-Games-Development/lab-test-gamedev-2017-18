42047fe7b588c1615ab6787b39e030591961e72d
B00082154 Dylan Byrne <B00082154@student.itb.ie>
2018-01-12 15:58:36 +0000

    Game Dev Test#

---
+++ Assets/_Scripts/ShieldController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShieldController{
+    private ShieldView view;
+    private ShieldModel model;
+
+    private Text Strength;
+
+    public void SetView(ShieldView view)
+    {
+        this.view = view;
+    }
+
+    public void SetModel(ShieldModel model)
+    {
+        this.model = model;
+    }
+
+    public void SetStrength(Text strength)
+    {
+        this.Strength = Strength;
+    }
+
+    private int GetShieldDamage()
+    {
+        string damageString = Strength.text;
+        
+            if(damageString.Length > 90)
+        {
+            damageString = "100";
+        }
+        int damage = int.Parse(damageString);
+        return damage;
+    }
+}
---
+++ Assets/_Scripts/ShieldView.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShieldView{
+    private ShieldModel model;
+    private Text Strength;
+    private Text isRepairable;
+
+    private RawImage[] shieldStrength;
+
+    public void SetModel(ShieldModel model)
+    {
+        this.model = model;
+    }
+
+    public void SetText(Text Strength, Text isRepairable)
+    {
+        this.Strength = Strength;
+        this.isRepairable = isRepairable;
+    }
+
+    public void SetImage(RawImage shieldStrength)
+    {
+
+    }
+
+    public void TextAfterChanges()
+    {
+        this.Strength.text = model.GetStrength().ToString();
+        this.isRepairable.text = model.IsRepairable().ToString();
+    }
+
+}
