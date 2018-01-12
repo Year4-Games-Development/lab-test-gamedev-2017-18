264b3b807603475601172cd99c6b50fc3189655c
B00069215 Ross Kennedy <B00069215@student.itb.ie>
2018-01-12 16:11:28 +0000

    uploading labtest

---
+++ Assets/_Scripts/ShieldManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShieldManager : MonoBehaviour {
+
+	private ShieldModel model;
+	private ShieldView view;
+	private ShieldController controller;
+
+	public Text StrengthText;
+	public Text TrueOrFalse;
+	public Button Damage1;
+	public Button Damage10;
+	public Button Tick;
+
+	void Start(){
+		//model
+		model = new ShieldModel ();
+
+		//view
+		view = new ShieldView ();
+		view.SetModel (model);
+		view.SetUITexts (StrengthText, TrueOrFalse);
+		view.Refresh ();
+
+		//controller
+		controller = new ShieldController ();
+		controller.SetModel (model);
+		controller.SetView (view);
+		controller.SetButtons (Damage1, Damage10);
+	}
+
+	public void Button_Damage1Click(){
+		controller.EVENT_Damage1 ();
+	}
+
+	public void Button_DamageTenClick(){
+		controller.EVENT_Damage10 ();
+	}
+}
---
+++ Assets/_Scripts/ShieldView.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShieldView {
+
+	private ShieldModel model;
+	private Text StrengthText;
+	private Text TrueOrFalse;
+
+	public void SetModel(ShieldModel model){
+		this.model = model;
+	}
+
+
+	public void SetUITexts(Text StrengthText, Text TrueOrFalse){
+		this.StrengthText = StrengthText;
+		this.TrueOrFalse = TrueOrFalse;
+	}
+
+	public void Refresh(){
+		this.StrengthText.text = model.GetStrength ().ToString ();
+		this.TrueOrFalse.text = model.IsRepairable ().ToString ();
+	}
+
+}
