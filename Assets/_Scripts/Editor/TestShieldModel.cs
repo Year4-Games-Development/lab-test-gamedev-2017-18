070642ffd76b1f947a05844739e72b85a6199937
B00046880 Charles John <B00046880@student.itb.ie>
2018-01-12 15:59:53 +0000

    ShiledTestB00046880

---
+++ Assets/_Scripts/Editor/TestShieldModel.cs
@@ -0,0 +1,177 @@
+﻿using NUnit.Framework;
+
+public class TestShieldModel {
+
+	/// <summary>
+	/// Question 2
+	/// </summary>
+	[Test]
+	public void TestWhenStrengthOneHundred() {
+
+		//Arrange
+		ShieldModel sm = new ShieldModel ();
+
+		int expectedResult = 100;
+
+		//Act
+		int result = sm.GetStrength();
+
+		//Assert
+		Assert.AreEqual(expectedResult, result);
+
+	}
+
+
+	[Test]
+	public void TestWhenRepairQuantityTen() {
+
+		//Arrange
+		ShieldModel sm = new ShieldModel ();
+
+		int expectedResult = 10;
+
+		//Act
+		int result = sm.RepairQuantity;
+
+		//Assert
+		Assert.AreEqual(expectedResult, result);
+
+	}
+
+	[Test]
+	public void TestWhenRepairThresholdTwentyFive() {
+
+		//Arrange
+		ShieldModel sm = new ShieldModel ();
+
+		int expectedResult = 25;
+
+		//Act
+		int result = sm.RepairableThreshold;
+
+		//Assert
+		Assert.AreEqual(expectedResult, result);
+
+	}
+
+
+	[Test]
+	public void TestWhenIsRepairAbleTrue() {
+
+		//Arrange
+		ShieldModel sm = new ShieldModel ();
+
+		//Act
+		bool result = sm.IsRepairable();
+
+		//Assert
+		Assert.True(result);
+
+	}
+
+	/// <summary>
+	/// Question 3
+	/// </summary>
+
+	[Test]
+	public void TestAfterOneDamageOneReturnTrue() {
+
+		//Arrange
+		ShieldModel sm = new ShieldModel ();
+		int damageQuantity = 1;
+		int expectedResult = 99;
+
+		//Act
+		bool damageSuccess1 = sm.TakeDamage(damageQuantity);
+
+		int result = sm.GetStrength ();
+
+		//Assert
+		Assert.True(damageSuccess1);
+		Assert.AreEqual(expectedResult, result);
+
+	}
+
+	[Test]
+	public void TestAfterOneDamageTenReturnTrue() {
+
+		//Arrange
+		ShieldModel sm = new ShieldModel ();
+		int damageQuantity = 10;
+		int expectedResult = 90;
+
+		//Act
+		bool damageSuccess1 = sm.TakeDamage(damageQuantity);
+
+		int result = sm.GetStrength ();
+
+		//Assert
+		Assert.True(damageSuccess1);
+		Assert.AreEqual(expectedResult, result);
+
+	}
+
+	[Test]
+	public void TestAfterEightDamageTenReturnFalse() {
+
+		//Arrange
+		ShieldModel sm = new ShieldModel ();
+		int damageQuantity = 10;
+		int expectedResult = 20;
+
+		//Act
+		bool damageSuccess1 = sm.TakeDamage(damageQuantity);
+		bool damageSuccess2 = sm.TakeDamage(damageQuantity);
+		bool damageSuccess3 = sm.TakeDamage(damageQuantity);
+		bool damageSuccess4 = sm.TakeDamage(damageQuantity);
+		bool damageSuccess5 = sm.TakeDamage(damageQuantity);
+		bool damageSuccess6 = sm.TakeDamage(damageQuantity);
+		bool damageSuccess7 = sm.TakeDamage(damageQuantity);
+		bool damageSuccess8 = sm.TakeDamage(damageQuantity);
+
+		bool damageSuccess = sm.TakeDamage(damageQuantity);
+
+		int result = sm.GetStrength ();
+
+		//Assert
+		Assert.False(sm.TakeDamage(damageQuantity));
+		Assert.AreEqual(expectedResult, result);
+	}
+
+	[Test]
+	public void TestAfterOneDamageNinetyReturnFalse() {
+
+		//Arrange
+		ShieldModel sm = new ShieldModel ();
+		int damageQuantity = 90;
+		int expectedResult = 20;
+
+		//Act
+		bool damageSuccess1 = sm.TakeDamage(damageQuantity);
+
+		int result = sm.GetStrength ();
+
+		//Assert
+		Assert.False(sm.TakeDamage(damageQuantity));
+		Assert.AreEqual(expectedResult, result);
+	}
+
+	[Test]
+	public void TestAfterOneDamageOneTickReturnTrue() {
+
+		//Arrange
+		ShieldModel sm = new ShieldModel ();
+		int damageQuantity = 11;
+		int expectedResult = 99;
+
+		//Act
+		bool damageSuccess1 = sm.TakeDamage(damageQuantity);
+
+		int result = sm.GetStrength ();
+
+		//Assert
+		Assert.True(sm.TakeDamage(damageQuantity));
+		Assert.AreEqual(expectedResult, result);
+	}
+
+}
---
+++ Assets/_Scripts/ShieldController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldController : MonoBehaviour {
+
+	private ShieldModel model;
+	private ShieldView view;
+
+	public void CheckStrengthValue(){
+
+		int strenght = model.GetStrength ();
+
+		if (strenght == 0 && strenght < 10) {
+		
+			view.SetUITexts (model.GetStrength(), model.IsRepairable() );
+
+
+			//view.SetImageSprite (0);
+		}
+		else if(strenght == 10 && strenght < 20){
+			
+		}
+		else if(strenght == 20 && strenght < 30){
+
+		}
+		else if(strenght == 30 && strenght < 40){
+
+		}
+		else if(strenght == 40 && strenght < 50){
+
+		}
+		else if(strenght == 50 && strenght < 60){
+
+		}
+		else if(strenght == 60 && strenght < 70){
+
+		}
+		else if(strenght == 70 && strenght < 80){
+
+		}
+		else if(strenght == 80 && strenght < 90){
+
+		}
+		else if(strenght == 90 && strenght <= 100){
+
+		}
+	}
+
+}
