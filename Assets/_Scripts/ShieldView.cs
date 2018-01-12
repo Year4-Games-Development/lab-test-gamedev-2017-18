cbacafa484afe89c2990ebae56265cc4d37adb69
B00062739 Adem Slavotic <B00062739@student.itb.ie>
2018-01-12 16:08:16 +0000

    labtest

---
+++ Assets/_Scripts/ShieldView.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShieldView {
+
+	private ShieldModel model;
+	private Text textStrengthLabel;
+	private Text textStrengthValue;
+	private Text textRepairableLabel;
+	private Text textRepairableValue;
+
+	public void SetModel(ShieldModel model){
+		this.model = model;
+	}
+
+	public void SetUITexts(Text textStrengthLabel, Text textStrengthValue, 
+		Text textRepairableLabel, Text textRepairableValue){
+
+		this.textStrengthLabel = textStrengthLabel;
+		this.textStrengthValue = textStrengthValue;
+		this.textRepairableLabel = textRepairableLabel;
+		this.textRepairableValue = textRepairableValue;
+	}
+
+	public void Refresh(){
+		this.textStrengthLabel.text = model.GetStrength ().ToString ();
+		this.textStrengthValue.text = model.GetStrength ().ToString ();
+		this.textRepairableValue.text = model.IsRepairable().ToString ();
+		this.textRepairableLabel.text = model.IsRepairable ().ToString ();
+	}
+
+}
